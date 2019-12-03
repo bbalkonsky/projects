a30ceb8266b7c3a7a1bd92cfd21a017627fcf26b
bbalkonsky <bbalkonsky@gmail.com>
2019-12-03 16:14:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 import React from 'react';
-import ProjectItem from "./components/ProjectItem";
+import {dbprojects} from "./store/dataBase";
+import ProjectIstem from "./components/ProjectIstem";
 import 'antd/dist/antd.css';
 import { Row, Col } from 'antd';
-import axios from 'axios';
-
-async function getDataAxios() {
-    const response = await axios.get("http://localhost:4000/all");
-    projects = response.data;
-}
+// import axios from 'axios';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import ProjectItem from "./components/ProjectItem";
 
 function App() {
-    getDataAxios().then(res => console.log(projects));
     return (
         <div className="App">
-            <p>{projects.length}</p>
-            {projects.length > 0 &&
-                <Row>
-                    <Col span={12} offset={6}>
-                        {projects.map(project =>
-                            <ProjectItem
-                                id={project.id}
-                                header={project.title}
-                                description={project.description}
-                                tags={project.tags}
-                                colors={project.colors}
-                                key={project.id}/>
-                        )}
-                    </Col>
-                </Row>
-            }
+            <Row>
+                <Col span={12} offset={6}>
+                    <Router>
+                        <Switch>
+                            <Route path="/project/:id">
+                                <ProjectItem />
+                            </Route>
+                            <Route path="/">
+                                {dbprojects.map((project, idx) =>
+                                    <ProjectIstem
+                                        id={project.id}
+                                        header={project.title}
+                                        description={project.description}
+                                        tags={project.tags}
+                                        colors={project.colors}
+                                        key={idx} />
+                                )}
+                            </Route>
+                        </Switch>
+                    </Router>
+                </Col>
+            </Row>
         </div>
     );
 }
 
 export default App;
-
-let projects = [];
