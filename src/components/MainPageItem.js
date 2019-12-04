d593cc30221113df708deb0a0b03127eb12ab643
bbalkonsky <bbalkonsky@gmail.com>
2019-12-04 16:44:37 +0500

    Update MainPageItem.js

--- src/components/MainPageItem.js
+++ src/components/MainPageItem.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {
-    Link
-} from "react-router-dom";
-import {Card, Typography} from "antd";
+import { Link } from "react-router-dom";
+import {Button, Card, Col, Typography} from "antd";
 const { Title } = Typography;
 
 
 function MainPageItem(props) {
     return (
-        <Card>
-            <Title level={3}><Link to={'project/' + props.id}>{props.header}</Link></Title>
-            <p>{props.description}</p>
-        </Card>
+        <Col span={8}>
+            <Card style={{marginBottom: '30px'}}>
+                <Title level={3}><Link to={'project/' + props.id}>{props.header}</Link></Title>
+                <p>{props.description}</p>
+
+                <Button.Group>
+                    <Button disabled={!props.prodUrl}><a href={props.prodUrl}>Production-сервер</a></Button>
+                    <Button><a href={props.devUrl}>Staging-сервер</a></Button>
+                    <Button><a href={props.gitUrl}>GitLab</a></Button>
+                </Button.Group>
+            </Card>
+        </Col>
     );
 }
 
