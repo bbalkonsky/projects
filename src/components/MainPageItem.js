9beeb768194a5d1a9a7c7c7462930744ea91264f
bbalkonsky <bbalkonsky@gmail.com>
2019-12-04 11:29:40 +0500

    Update and rename ProjectsItem.js to MainPageItem.js

--- src/components/MainPageItem.js
+++ src/components/MainPageItem.js
@@ -2,22 +2,17 @@ import React from 'react';
 import {
     Link
 } from "react-router-dom";
-import {Card, Tag, Typography} from "antd";
+import {Card, Typography} from "antd";
 const { Title } = Typography;
 
 
-function ProjectItem(props) {
+function MainPageItem(props) {
     return (
         <Card>
             <Title level={3}><Link to={'project/' + props.id}>{props.header}</Link></Title>
             <p>{props.description}</p>
-            <div>
-                {props.tags.map((item, idx) =>
-                    <Tag color={props.colors[idx]} key={idx}>{item}</Tag>
-                )}
-            </div>
         </Card>
     );
 }
 
-export default ProjectItem;
+export default MainPageItem;
